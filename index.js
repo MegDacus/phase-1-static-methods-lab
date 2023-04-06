007a3ae4741b02bd849c7e7369dd6c90801eced8
Megan Dacus <meganjustice0609@gmail.com>
2023-04-06 18:41:48 +1000

    Assignment Completed

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 class Formatter {
-  //add static methods here
-}
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-z'' -]/gi, '');
+  }
+
+  static titleize(string) {
+    let excludeWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    
+    return this.capitalize(string.replace(/\b\w+\b/g, 
+    word => excludeWords.includes(word) ? word : this.capitalize(word)))
+
+}}
